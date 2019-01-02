0ac3704a14d8ccb1ed442d9f711b891e5ee0b0ae
Indraj Bahra <indraj.bahra@googlemail.com>
2019-01-02 21:11:30 +0000

    added content to games webpage and attempted to solve javascript JSON wiki search bar issue

--- js/scripts.js
+++ js/scripts.js
@@ -9,106 +9,99 @@ function greeting() {
 
 welcome.addEventListener("click", greeting);
 
-"use strict";
+
+
 
 //search bar
+//"use strict";
+
 (function(){
-  //create xhr that handles API call
+  // creates a new object called xhr
+  // which will handle the API call
   let xhr = new XMLHttpRequest();
+  // console.log(`Current readyState: ${xhr.readyState}`);
 
   let queryBox = document.getElementById("wikiQuery");
   let searchForm = document.getElementById("searchForm");
   let demoJSON = document.getElementById("demo");
 
-  //base for the request url
-  let baseURL = "http://en.wikipedia.org/w/api.php? \
-                  format=json& \
-                  action=query& \
-                  generator=search& \
-                  gsrnamespace=0& \
-                  gsrlimit=10& \
-                  prop=info|extracts|langlinks|pageimages& \
-                  inprop=url& \
-                  exintro& \
-                  explaintext& \
-                  exsentences=1& \
-                  exlimit=max& \
-                  llpropp=url& \
-                  lllimit=max& \
-                  piprop=thumbnail|name& \
-                  origin=*& \
-                  origin=*& \
-                  gsrsearch=";
+  // constructs the base for the request url
+  let baseURL = "https://en.wikipedia.org/w/api.php? \
+                format=json& \
+                action=query& \
+                generator=search& \
+                gsrnamespace=0& \
+                gsrlimit=10& \
+                prop=info|extracts|langlinks|pageimages& \
+                inprop=url& \
+                exintro& \
+                explaintext& \
+                exsentences=1& \
+                exlimit=max& \
+                llprop=url& \
+                lllimit=max& \
+                piprop=thumbnail|name& \
+                origin=*& \
+                gsrsearch=";
 
-  /* api sandbox url
-
-  */
+/*
+API Sandbox url
+https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&generator=search&prop=extracts%7Clanglinks%7Cpageimages&gsrlimit=10&gsrnamespace=0&exintro&explaintext&exsentences=1&exlimit=max&llprop=url&lllimit=max&piprop=thumbnail|name&origin=*&gsrsearch=kittens
+Request url
+https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&prop=extracts%7Clanglinks%7Cpageimages&gsrlimit=10&gsrnamespace=0&exintro&explaintext&exsentences=1&exlimit=max&llprop=url&lllimit=max&piprop=thumbnail|name&origin=*&gsrsearch=kittens
+*/
 
   function gatherData(data) {
+    // console.log(data);
+    // initialise some variables
     let theData = "";
-    let langLink = "";
-    let img ="<img>";
-    const languages = ["en"];
+    let langLinks = "";
+    let img = "<img>";
+    const languages = [];
     let k;
     let key;
-    //loop through the results pages by pageid
+    // loop through the result pages by pageid
     for(key in data.query.pages) {
       let tmp = data.query.pages[key];
       if (tmp.thumbnail) {
-        img = `<img src="${tmp.thumbnail.source}" alt="${tmp.title}"; `;
+        img = `<img src="${tmp.thumbnail.source}" alt="${tmp.title}"> `;
       }
-      let title = '<strong><a href="${tmp.fullurl}">${tmp.title}</a></strong>';
-      let extract = '<span class="txt">${tmp.extract}</span>';
-      let langlinks = "";
+      let title = `<strong><a href="${tmp.fullurl}">${tmp.title}</a></strong>`;
+      let extract = `<span class="txt">${tmp.extract}</span>`;
+      let langLinks = "";
       for (k in tmp.langlinks) {
         if (languages.includes(tmp.langlinks[k].lang)) {
-          langLinks += '<a href=${tmp.langlinks[k].url}>${tmp.langlinks[k].lang}</a> ';
+          langLinks += `<a href=${tmp.langlinks[k].url}>${tmp.langlinks[k].lang}</a> `;
         }
       }
-      theData += '<li>${img} ${title} ${extract} <span class="langs">${langLinks}</span></li> ';
+      theData += `<li>${img} ${title} ${extract} <span class="langs">${langLinks}</span></li>`;
     }
     demoJSON.innerHTML = theData;
   }
 
-  //the API call is triggered as soon as the users enter and submit their seach option
+  // the API call is triggered once the user submits a query
   searchForm.addEventListener("submit", function(ev){
-    //complete the request url
+    // complete the request url
     let wiki = baseURL + queryBox.value;
-    //open a connection to the requested API url
+    // open a connection to the requested API url
     xhr.open("GET", wiki, true);
-    //be polite to wikipedia
+    // be polite to Wikipedia
     xhr.setRequestHeader('Api-User-Agent', 'Example/1.0');
-    //send the request enetred by users off
+    // send off that request
     xhr.send();
-    // if the response was ok, handle the response data using the gatherData functions
+    // if the response was ok, handle the response data using the gatherData function
     xhr.onreadystatechange = function() {
+      // console.log(`Current readyState: ${xhr.readyState}`);
       if (xhr.readyState === 4 && xhr.status === 200) {
-        //parse the response json
+        // parse the response JSON
         let response = JSON.parse(xhr.responseText);
-        //deal with the parsed JSON theData
+        // deal with the parsed JSON data
         gatherData(response);
       }
     };
-    //clear the search box after searchForm
-    queryBox.value ="";
+    // clear the search box
+    queryBox.value = "";
     ev.preventDefault();
   }, false);
+
 }());
-
-
-
-//slide show
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.lentgh) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 2000);
-}
